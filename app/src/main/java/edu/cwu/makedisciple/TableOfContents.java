a1e45bb4ee6dd997647624756e8420e7ee97c5aa
Jesse Calvert <jesseclvrt1@gmail.com>
2019-01-10 12:46:41 -0800

    refactored mainactivity to table of contents, added buttons

--- app/src/main/java/edu/cwu/makedisciple/TableOfContents.java
+++ app/src/main/java/edu/cwu/makedisciple/TableOfContents.java
@@ -3,11 +3,11 @@ package edu.cwu.makedisciple;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TableOfContents extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.table_of_contents);
     }
 }
