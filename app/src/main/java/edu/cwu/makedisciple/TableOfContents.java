b77ddefb3db699e56ac8ea90de9b26f7925edc1a
Carlos <valdivia206@outlook.com>
2019-01-17 19:26:30 -0800

    Button Layout Changes
    changed the layout of the buttons also added an intent to the About button

--- app/src/main/java/edu/cwu/makedisciple/TableOfContents.java
+++ app/src/main/java/edu/cwu/makedisciple/TableOfContents.java
@@ -1,7 +1,10 @@
 package edu.cwu.makedisciple;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TableOfContents extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class TableOfContents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.table_of_contents);
+
+        Button aboutButton = (Button) findViewById(R.id.AboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TableOfContents.this,About.class));
+            }
+        });
     }
 }
