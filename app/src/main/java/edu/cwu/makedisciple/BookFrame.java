eda4ef3f55109a92677b28d2aba91db8a93f58bc
Carlos <valdivia206@outlook.com>
2019-01-22 13:56:19 -0800

    Functional Abbreviations
    new intent for abbreviations

--- app/src/main/java/edu/cwu/makedisciple/BookFrame.java
+++ app/src/main/java/edu/cwu/makedisciple/BookFrame.java
@@ -1,7 +1,10 @@
 package edu.cwu.makedisciple;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BookFrame extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class BookFrame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_frame);
+
+        //Intent to the abbreviations
+        Button abbreviationButton = (Button) findViewById(R.id.AbbButton);
+        abbreviationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BookFrame.this,Abbreviations.class));
+            }
+        });
+
     }
 }
