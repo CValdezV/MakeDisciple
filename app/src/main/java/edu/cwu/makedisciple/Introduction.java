b4cbdcbba98e5342318f9b35ede3612f4d376fb1
Jesse Calvert <jesseclvrt1@gmail.com>
2019-01-28 09:54:05 -0800

    intro xml finish, error in Introduction.java

--- app/src/main/java/edu/cwu/makedisciple/Introduction.java
+++ app/src/main/java/edu/cwu/makedisciple/Introduction.java
@@ -1,7 +1,9 @@
 package edu.cwu.makedisciple;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
@@ -11,7 +13,8 @@ public class Introduction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
-        TextView text = findViewById(R.id.Introduction);
-        text.setMovementMethod(new ScrollingMovementMethod());
+        //Toolbar setup
+        Toolbar bar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(bar);
     }
 }
