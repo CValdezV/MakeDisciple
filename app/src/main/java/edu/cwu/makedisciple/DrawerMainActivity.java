93d19047aff4062c2cc440170245064d8e5ccf73
Jesse Calvert <jesseclvrt1@gmail.com>
2019-02-14 17:17:15 -0800

    renamed files, deleted unused files, changed some strings

--- app/src/main/java/edu/cwu/makedisciple/DrawerMainActivity.java
+++ app/src/main/java/edu/cwu/makedisciple/DrawerMainActivity.java
@@ -55,7 +55,7 @@ public class DrawerMainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer_main, menu);
+        getMenuInflater().inflate(R.menu.action_bar, menu);
         return true;
     }
 
